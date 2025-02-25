63445c43fc6c2baea17b34c3a16542797100aea4
WildBunnie <wildbunnie2003@gmail.com>
2025-02-25 09:46:59 +0000

    went back to using default items instead of randomly picking them

--- world/dark_souls_2/__init__.py
+++ world/dark_souls_2/__init__.py
@@ -10,6 +10,13 @@ from .Options import DS2Options
 
 class DS2Location(Location):
     game: str = "Dark Souls II"
+    default_items: list[str]
+
+    def __init__(self, player, name, code, parent_region, default_items):
+        self.default_items = default_items
+        super(DS2Location, self).__init__(
+            player, name, code, parent_region
+        )
 
 class DS2Item(Item):
     game: str = "Dark Souls II"
@@ -37,7 +44,7 @@ class DS2World(World):
             region = self.create_region(region_name)
             for location_data in location_table[region_name]:
                 if location_data.ngp and not self.options.enable_ngp: continue
-                location = self.create_location(location_data.name, region)
+                location = self.create_location(location_data.name, region, location_data.default_items)
                 region.locations.append(location)
             regions[region_name] = region
             self.multiworld.regions.append(region)
@@ -89,8 +96,8 @@ class DS2World(World):
     def create_region(self, name):
         return Region(name, self.player, self.multiworld)
 
-    def create_location(self, name, region):
-        return DS2Location(self.player, name, self.location_name_to_id[name], region)
+    def create_location(self, name, region, default_items):
+        return DS2Location(self.player, name, self.location_name_to_id[name], region, default_items)
 
     def create_items(self):
         pool : list[DS2Item] = []
@@ -107,21 +114,19 @@ class DS2World(World):
             item = self.create_item(progression_item)
             pool.append(item)
 
-        assert len(pool) <= max_pool_size , "item pool cannot fit all the progression items" 
+        assert len(pool) <= max_pool_size, "item pool cannot fit all the progression items" 
         
-        # initial attempt at filling the item pool
-        items_in_pool = [item.name for item in pool]
-        for _ in range(max_pool_size - len(pool)):
+        # initial attempt at filling the item pool with the default items
+        for location in self.multiworld.get_unfilled_locations(self.player):
             
-            item_data = random.choice(item_list)
-            
+            item_name = random.choice(location.default_items)
+            item_data = next((item for item in item_list if item.name == item_name), None)
+            assert item_data, "location's default item not in item list"
+
             # skip unwanted items
             if item_data.skip: continue
-            # dont allow duplicates
-            if item_data.category not in repetable_categories and item_data.name in items_in_pool: continue
 
             item = self.create_item(item_data.name)
-            items_in_pool.append(item_data.name)
             pool.append(item)
 
         # fill the rest of the pool with filler items
@@ -130,7 +135,7 @@ class DS2World(World):
             item = self.create_item(random.choice(filler_items).name)
             pool.append(item)
 
-        assert len(pool) == max_pool_size , "item pool is under-filled or over-filled"
+        assert len(pool) == max_pool_size, "item pool is under-filled or over-filled"
 
         self.multiworld.itempool += pool
 
